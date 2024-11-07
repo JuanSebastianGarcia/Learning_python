aa3040da20975c22543dd1250c727a24392bb70b
Juan Sebastian Garcia Alzate <117672609+JuanSebastianGarcia@users.noreply.github.com>
2024-11-07 12:39:01 -0500

    se descarto la extraccion de las reseñas, debido a la seguridad que tiene la plataforma amazon para los bots, pero se descarto temporalmente. ademas, se agrego un pequeño proceso que permite pasar las paginas de una lista de productos para extraer mas datos

--- Application/WebScraping/proyecto2/Main.py
+++ Application/WebScraping/proyecto2/Main.py
@@ -13,18 +13,31 @@ los datos que se extraeran son
 Con base en los datos extraidos analizaremos
     1-Promedio de precios de los productos.
     2-Distribución de precios para visualizar la variación.
+
+
+
+    Aprendizajes
+    *el bloqueo para ataques dos de cada plataforma podra ser una restriccion en la tecnica de web scraping
+    *hay web scraping dinamico y estatico
+    *la seguridad no solo de dos, es el mayor obstaculo
+    *la puesta de informacion erronea, con o sin culpa, afecta los resultados
 """
 
 from selenium import webdriver  # Librería para la interacción dinámica con las páginas web
 from selenium.webdriver.common.by import By  # Módulo para seleccionar elementos en la página por diferentes tipos de localizadores
 from selenium.webdriver.common.keys import Keys  # Módulo para simular la pulsación de teclas como Enter
+from selenium.webdriver.support import expected_conditions as EC
 import time  # Módulo para gestionar pausas y esperas en el flujo del programa
 import os  # Módulo para interactuar con el sistema operativo y gestionar rutas
 from selenium.webdriver.chrome.service import Service  # Módulo para iniciar y manejar el servicio de ChromeDriver
 from bs4 import BeautifulSoup # Modulo para interactuar con el contenido html 
 import pandas as pd#libreria para manejar dataframes
 import matplotlib.pyplot as plt#libreria para imprimmir graficas
-import requests
+import requests#Modulo para solicitudes http
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+
 
 class Proyecto2():
     """
@@ -41,8 +54,7 @@ class Proyecto2():
     """ 
 
     #dataframe para almacenar los datos de los productos
-    data = pd.DataFrame(columns=['Producto','Precio','Estrellas','Reseñas'])
-
+    data = pd.DataFrame(columns=['Producto','Precio','Estrellas'])
 
 
     #metodo constructor
@@ -127,20 +139,14 @@ class Proyecto2():
 
             if titulo and estrellas and precio:
                 
-                reseñas = self.extraer_reseñas_producto(producto)
-
                 fila = {
                     'Producto': titulo.text.strip(),
                     'Precio': precio.text.strip(),
                     'Estrellas': estrellas['aria-label'][:3].strip(),
-                    'Reseñas':reseñas
                 }
                  # Agregar la fila al DataFrame
                 self.data = pd.concat([self.data, pd.DataFrame([fila])], ignore_index=True)
 
-       
-
-        self.guardar_csv()#almacenar csv
 
 
 
@@ -174,6 +180,8 @@ class Proyecto2():
         return reseñas
             
 
+
+
     #extraer las reseñas de un producto
     def extraer_reseñas(self,link:str):
         """
@@ -200,6 +208,8 @@ class Proyecto2():
         else:
             print('enlace fallido')
 
+        time.sleep(5)#esperamos un segundo de espera
+
         return ' '.join(reseñas)
 
 
@@ -207,7 +217,7 @@ class Proyecto2():
     #almacenar un archivo csv
     def guardar_csv(self):
         """
-        Almacenar los datos obtenidos en un archivo csv
+        Almacenar los datos obtenidos en un archivo csv. 
         """
         #extraer la direccion en la que se almacenara el documento
         base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -257,6 +267,24 @@ class Proyecto2():
         plt.tight_layout()  # Ajuste automático para que las etiquetas no se superpongan
         plt.show()
 
+    #pasar de pagina en amazon
+    def pasar_pagina(self):
+        """
+            pasar pagina en la pagina de amazon. para extraer mas datos tenemos que recorrer las 
+            diferentes paginas de productos de amazon
+        """
+        #se hace scroll hasta abajo
+        self.driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+
+        boton = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.CLASS_NAME, 's-pagination-item.s-pagination-next.s-pagination-button.s-pagination-separator'))
+        )
+
+    
+        time.sleep(2)
+
+
+
 
     #iniciar con la ajecucion del programa
     def iniciar(self):
@@ -267,13 +295,25 @@ class Proyecto2():
             1.se carga el driver para navegar en google
             2.se hace la busqueda del producto deseado
             3.se extraen los datos
-            4.se procesan los datos
+            4.se almacenan los datos
+            5.se procesan los datos
         """
         self.tipo_producto = input('ingresa el producto')#pedimos el dato al usuario
 
         self.cargar_driver()#driver para navegar en google
+
         self.realizar_busqueda()#busqueda del producto que se analizara
-        self.extraer_elementos_producto()#se extraen los datos de esos productos
+
+        #con el for, se pasaran 5 paginas para extraer mas datos
+        for i in range(5):
+            self.extraer_elementos_producto()#se extraen los datos de esos productos
+            
+            self.guardar_csv()#guardar los datos en el archivo csv
+
+            self.pasar_pagina()#nos aseguramos de seguir a la siguiente pagina de datos
+
+
+
 
         self.procesar_precios()#calcular el promedio de precios 
 
